db9397260af67e682bc48b55b3b2e4402a3e6f67
jiangyi15 <jiangyi15@mails.ucas.ac.cn>
2024-06-10 22:17:07 +0800

    ci: fixed ci error

--- tf_pwa/config_loader/tests/test_particle_function.py
+++ tf_pwa/config_loader/tests/test_particle_function.py
@@ -11,7 +11,7 @@ def test_particle(toy_config):
     assert f(m).shape == (5, 6)
     g = toy_config.get_particle_function("R_BC", d_norm=True)
     g.phsp_fractor(m)
-    cached_g = g.cached_fun(m)
+    cached_g = g.cached_call(m)
     assert np.allclose(g(m).numpy(), cached_g().numpy())
     g.density(m)
     assert g(m).shape == (5, 6)
